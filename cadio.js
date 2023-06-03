8b4b1dbdd5f40b4ac5270bdc5fb21c91ec1afb99
aiyus <aiyusboss@gmail.com>
2023-06-03 16:10:40 +0100

    another

--- cadio.js
+++ cadio.js
@@ -23,4 +23,26 @@ let ule = document.querySelector("ul");
 ule.insertAdjacentHTML("beforebegin", html);
 const adiv = document.querySelector(".adiv");
 adiv.children[0].remove();
-function generatecard(name, age, height) {}
+function generatecard(name, age, height) {
+  let html = `<div class='playcard'>
+  <h2> ${name} - ${age}</h2>
+  <p> their height is ${height} and  ${age} years old. in dog years this person would be ${
+    age * 7
+  } that will be a tall dog</p>
+  <button class='delete'>delete</button>
+  </div>`;
+  return html;
+}
+const newdiv = document.createElement("div");
+newdiv.classList = "cards";
+let playcards = generatecard("aiyus", 12, 150);
+playcards += generatecard("edna", 12, 150);
+playcards += generatecard("williams", 12, 150);
+playcards += generatecard("testimony", 12, 150);
+newdiv.innerHTML = playcards;
+myDiv.insertAdjacentElement("beforebegin", newdiv);
+let buttons = document.querySelectorAll(".delete");
+function deletes(event) {
+  classList.playcard.event.remove();
+}
+buttons.forEach((button) => button.addEventListener("click", deletes));
